c194289abc3719783b0ca7b9b91ebc68bb4e86d3
Aman Manwani <aman04.manwani@gmail.com>
2024-06-05 10:25:49 +0530

    added comment functionality

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -12,7 +12,7 @@ const Home = async() => {
     <>
       <h1 className="head-text text-left">Home</h1>
 
-      <section>
+      <section className='mt-9 flex flex-col gap-10'>
         {result.posts.length === 0 ? (
           <p className="no-result">No posts found</p>
           ) : (
